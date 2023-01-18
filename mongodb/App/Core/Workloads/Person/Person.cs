f39104231f5f9081d55fed7fb9aab3a3ac166daa
S1mple133 <pavelescu_darius@yahoo.com>
2023-01-18 14:14:02 +0100

    #3: added birthdate to person

--- mongodb/App/Core/Workloads/Person/Person.cs
+++ mongodb/App/Core/Workloads/Person/Person.cs
@@ -7,6 +7,7 @@ public sealed class Person : EntityBase
 {
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
+    public DateTime BirthDate { get; set; } = default!;
     public Location BirthPlace { get; set; } = default!;
     
     public List<ObjectId> Children { get; set; } = default!;
