12f280b35dca445755db61171ddd2cba8ce0c546
NiklasNeudorfer <nikitrash93@gmail.com>
2023-01-25 11:18:41 +0100

    fixed routing & sidebar improvements

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PersonDetailComponent } from './components/person-detail/person-detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PersonListComponent} from './components/person-list/person-list.component';
+import {AllRelationsComponent} from './components/all-relations/all-relations.component';
+import {PersonDetailComponent} from './components/person-detail/person-detail.component';
+import {NgxGraphModule} from "@swimlane/ngx-graph";
 
 @NgModule({
   declarations: [
     AppComponent,
+    PersonListComponent,
+    AllRelationsComponent,
     PersonDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
