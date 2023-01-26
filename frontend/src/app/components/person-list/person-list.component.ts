25e941f545c14f142963f1a287992f9547cbf672
Christoph <christoph.handel@icloud.com>
2023-01-26 11:32:43 +0100

    fix person-list view #2

--- frontend/src/app/components/person-list/person-list.component.ts
+++ frontend/src/app/components/person-list/person-list.component.ts
@@ -2,25 +2,47 @@ import {Component, OnInit} from '@angular/core';
 import {Person} from "../../models/person.model";
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-
+interface IDictionary {
+  [index: string]: Person | null;
+}
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
   styleUrls: ['./person-list.component.css']
 })
+
 export class PersonListComponent implements OnInit {
   personList: Person[] = [];
+  mother= {} as IDictionary;
+  father={}as IDictionary;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<Person[]>(environment.API_URL + "person").subscribe({ next: value => { this.personList = value}})
+    this.http.get<Person[]>(environment.API_URL + "person").subscribe({ next: value => {
+      this.personList = value
+      this.personList.forEach(p => {
+        if(p.fatherId){
+          this.getFatherString(p).subscribe({ next: value => { this.mother[p.id!] = value}})
+        }
+        else{
+          this.father[p.id!] = null
+        }
+        if(p.motherId){
+          this.getMotherString(p).subscribe({ next: value => { this.father[p.id!] = value}})
+        }
+        else{
+          this.mother[p.id!] = null
+        }
+      })
+    }})
   }
 
   getMotherString(person: Person) {
-    return person.motherId == null ? "Unknown" : person.motherId;
+     return this.http.get<Person>(environment.API_URL + "person/"+person.motherId);
   }
   getFatherString(person: Person) {
-    return person.motherId == null ? "Unknown" : person.motherId;
+    return this.http.get<Person>(environment.API_URL + "person/"+person.fatherId);
   }
 }
 
