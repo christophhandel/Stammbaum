61cd0b081fc5d2c49b953c0baf6fd0ae74e3c877
Christoph <christoph.handel@icloud.com>
2023-01-26 11:54:03 +0100

    person-detail add parents

--- frontend/src/app/components/person-detail/person-detail.component.ts
+++ frontend/src/app/components/person-detail/person-detail.component.ts
@@ -2,6 +2,8 @@ import {environment} from '../../../environments/environment';
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Person} from "../../models/person.model";
+import {ActivatedRoute} from "@angular/router";
+import {mode} from "d3";
 
 @Component({
   selector: 'app-person-detail',
@@ -15,20 +17,36 @@ export class PersonDetailComponent implements OnInit {
   mothers : Person[] = []
   fathers : Person[] = []
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.http.get<Person[]>(environment.API_URL + "person?sex=f").subscribe({ next: d => { this.mothers = d; }})
-    this.http.get<Person[]>(environment.API_URL + "person?sex=m").subscribe({ next: d => { this.fathers = d; }})
+    this.route.params.subscribe(params => {
+      if (params["id"]) {
+        this.http.get<Person>(environment.API_URL + "person/" + params["id"])
+          .subscribe(p => this.model = p);
+      }
+    });
+    this.http.get<Person[]>(environment.API_URL + "person?sex=f")
+      .subscribe({ next: d => { this.mothers = d; }})
+    this.http.get<Person[]>(environment.API_URL + "person?sex=m")
+      .subscribe({ next: d => { this.fathers = d; }})
   }
 
 
   onSubmit() {
     console.log(this.model)
-    this.http.post<Person>(environment.API_URL + "Person", this.model).subscribe({
+    if (this.model.id ==''){
+      this.http.post<Person>(environment.API_URL + "Person", this.model).subscribe({
+        next:d=>{
+          console.log(d)
+        }
+      })
+    }
+    else{
+      this.http.put<Person>(environment.API_URL + "Person/"+this.model.id, this.model).subscribe({
       next:d=>{
         console.log(d)
       }
-    })
+    })}
    }
 }
