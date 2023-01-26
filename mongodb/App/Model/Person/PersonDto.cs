2a50698b1dc3cf2f9435d72dca0732db4a30da65
Christoph <christoph.handel@icloud.com>
2023-01-26 11:31:38 +0100

    fix jsonpropertyName

--- mongodb/App/Model/Person/PersonDto.cs
+++ mongodb/App/Model/Person/PersonDto.cs
@@ -9,8 +9,8 @@ public sealed class PersonDto
     public string Firstname { get; set; } = default!;
     public string Lastname { get; set; } = default!;
     public string Sex { get; set; } = default!;
-    [JsonPropertyName("MotherId")]
+    [JsonPropertyName("motherId")]
     public string? Mother { get; set; } = default!;
-    [JsonPropertyName("FatherId")]
+    [JsonPropertyName("fatherId")]
     public string? Father { get; set; } = default!;
 }
