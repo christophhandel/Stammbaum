92e0851882e0b0c0f7ebd4a3ea85ad940e1a07e0
NiklasNeudorfer <nikitrash93@gmail.com>
2023-01-27 08:42:09 +0100

    #3 created a working example of a family tree graph

--- frontend/src/app/components/all-relations/all-relations.component.ts
+++ frontend/src/app/components/all-relations/all-relations.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Layout, Edge, Node, DagreNodesOnlyLayout} from '@swimlane/ngx-graph';
+import * as shape from 'd3-shape';
 
 @Component({
   selector: 'app-all-relations',
@@ -6,10 +8,53 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./all-relations.component.css']
 })
 export class AllRelationsComponent implements OnInit {
+  view: [number, number] = [1000, 1000];
+  curve = shape.curveBundle.beta(1);
 
-  constructor() { }
+  public layout: Layout = new DagreNodesOnlyLayout();
+  public links: Edge[] = [];
+  public nodes: Node[] = [];
+
+  clusters: { childNodeIds: string[]; id: string; label: string }[] = [];
+  edges: ({ id: string; source: string; target: string })[] = [];
+
+
+  constructor() {
+    this.getScreenSize();
+    this.showGraph();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?: any) {
+    this.view = [window.innerWidth * 9 / 12, window.innerHeight];
+  }
+
 
   ngOnInit(): void {
+  }
+
+  showGraph() {
+    this.nodes = [
+      {id: 'start', label: 'Root', position: {x: 0, y: 0}},
+      {id: '1', label: 'Neidi', position: {x: 1, y: 0}},
+      {id: '11', label: 'Nicole', position: {x: 1, y: 0}},
+      {id: '2', label: 'Neidis Gschwister 1', position: {x: 2, y: 0}},
+      {id: '3', label: 'Neidis Kind', position: {x: 3, y: 0}},
+      {id: '4', label: 'Neidis Gschwisters 1 Kind 2', position: {x: 4, y: 0}},
+      {id: '5', label: 'Neidis Kindes Kind', position: {x: 5, y: 0}},
+      {id: '6', label: 'Neidis Gschwisters 1 Kind 1', position: {x: 6, y: 0}}
+    ];
+
+    this.links = [
+      {source: 'start', target: '1', label: 'Parent'},
+      {source: 'start', target: '2', label: 'Parent'},
+      {source: '1', target: '3', label: 'Parent'},
+      {source: '11', target: '3', label: 'Parent'},
+      {source: '2', target: '4', label: 'Parent'},
+      {source: '2', target: '6', label: 'Parent'},
+      {source: '3', target: '5', label: 'Parent'},
+    ];
+
   }
 
 }
