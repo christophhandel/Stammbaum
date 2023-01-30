c30a396c3e9f7d9b4274f03f77459a4b4d2d8728
NiklasNeudorfer <nikitrash93@gmail.com>
2023-01-30 13:47:52 +0100

    #12 fixed scrolling for relations & 'centered it lol'

--- frontend/src/app/components/all-relations/all-relations.component.ts
+++ frontend/src/app/components/all-relations/all-relations.component.ts
@@ -29,12 +29,12 @@ export class AllRelationsComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?: any) {
-    this.view = [window.innerWidth * 9 / 12, window.innerHeight];
+    this.view = [window.innerWidth * 10 / 12, window.innerHeight * 10 /12];
   }
 
 
   ngOnInit(): void {
-    this.getScreenSize();
+    //this.getScreenSize();
     this.getData();
   }
 
@@ -54,28 +54,6 @@ export class AllRelationsComponent implements OnInit {
         this.getScreenSize();
       })
     })
-
-    // this.nodes = [
-    //   {id: 'start', label: 'Root', position: {x: 0, y: 0}},
-    //   {id: '1', label: 'Neidi', position: {x: 1, y: 0}},
-    //   {id: '11', label: 'Nicole', position: {x: 1, y: 0}},
-    //   {id: '2', label: 'Neidis Gschwister 1', position: {x: 2, y: 0}},
-    //   {id: '3', label: 'Neidis Kind', position: {x: 3, y: 0}},
-    //   {id: '4', label: 'Neidis Gschwisters 1 Kind 2', position: {x: 4, y: 0}},
-    //   {id: '5', label: 'Neidis Kindes Kind', position: {x: 5, y: 0}},
-    //   {id: '6', label: 'Neidis Gschwisters 1 Kind 1', position: {x: 6, y: 0}}
-    // ];
-
-    // this.links = [
-    //   {source: 'start', target: '1', label: 'Parent'},
-    //   {source: 'start', target: '2', label: 'Parent'},
-    //   {source: '1', target: '3', label: 'Parent'},
-    //   {source: '11', target: '3', label: 'Parent'},
-    //   {source: '2', target: '4', label: 'Parent'},
-    //   {source: '2', target: '6', label: 'Parent'},
-    //   {source: '3', target: '5', label: 'Parent'},
-    // ];
-
   }
 
   onNodeClick($event: any, node: any) {
