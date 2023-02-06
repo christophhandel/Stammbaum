de55b54315596488a0a83e605c30c79fc07c3946
S1mple133 <pavelescu_darius@yahoo.com>
2023-02-06 11:01:19 +0100

    fixed small bugs on stats site #46

--- frontend/src/app/components/stats/company-chart/company-chart.component.ts
+++ frontend/src/app/components/stats/company-chart/company-chart.component.ts
@@ -1,5 +1,4 @@
-import {ChangeDetectionStrategy, Component, NgModule, OnInit} from '@angular/core';
-import {multi, multi as multiA} from './data';
+import {ChangeDetectionStrategy, Component, NgModule, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Color, ScaleType} from "@swimlane/ngx-charts";
 import {RestService} from "../../../services/rest.service";
 import {ToastrService} from "ngx-toastr";
@@ -10,10 +9,10 @@ import {ToastrService} from "ngx-toastr";
   styleUrls: ['./company-chart.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CompanyChartComponent implements OnInit{
-  multi: {name: string,series: {name:string,value:number}[]}[] = []
+export class CompanyChartComponent implements OnInit, OnChanges {
+  multi: { name: string, series: { name: string, value: number }[] }[] = []
 
-  view: [number,number] = [700, 400];
+  view: [number, number] = [900, 400];
 
   // options
   showXAxis: boolean = true;
@@ -21,10 +20,10 @@ export class CompanyChartComponent implements OnInit{
   gradient: boolean = true;
   showLegend: boolean = true;
   showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Country';
+  xAxisLabel: string = 'Company';
   showYAxisLabel: boolean = true;
-  yAxisLabel: string = 'Population';
-  legendTitle: string = 'Years';
+  yAxisLabel: string = 'People';
+  legendTitle: string = 'Gender';
 
   colorScheme: Color = {
     domain: ['#D1495B', '#00798C'],
@@ -32,47 +31,43 @@ export class CompanyChartComponent implements OnInit{
     selectable: true,
     name: 'Customer Usage',
   };
+  test = "";
 
-  constructor(private restService: RestService,private  toastrService: ToastrService) {
+  constructor(private restService: RestService, private toastrService: ToastrService) {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+        this.loadData();
+    }
+
   ngOnInit(): void {
+    this.loadData();
+    }
+
+  private loadData() {
     this.restService.getAllCompanies().subscribe({
       next: value =>
       {
-        value.forEach(s => this.multi.push({
+        const arr: { name: string, series: { name: string, value: number }[] }[]=[];
+        value.forEach(s => arr.push({
           name: s.companyName,
           series: [
             {
-              name: "female",
+              name: "Female",
               value: s.femaleWorkers
             },
             {
-              name: "male",
+              name: "Male",
               value: s.maleWorkers
             }
           ]
-        }))
-        console.log(this.multi)
-        this.multi = [...this.multi]
+        }));
+        this.multi = [...arr]
       },
       error:()=>{
         this.toastrService.error("Die Daten konnten nicht geladen werden!")
       }
     })
-
-    }
-
-  onSelect(data: any[]): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  }
-
-  onActivate(data: any[]): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
-  }
-
-  onDeactivate(data: any[]): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
 
--- frontend/src/app/components/stats/stats.component.ts
+++ frontend/src/app/components/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {RestService} from "../../services/rest.service";
 
